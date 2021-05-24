daa8463010281ef4c03e4aed354634611df4b52d
Cássio Rubens <cassiorubensadv@gmail.com>
2021-05-24 15:59:07 -0300

    start the inventory report project

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,4 @@
+# 1 - Criar um método generate numa classe SimpleReport do módulo
+# inventory_report/reports/simple_report.py.
+# Esse método deverá receber dados numa lista contendo estruturas do tipo dict
+# e deverá retornar uma string formatada como um relatório.
