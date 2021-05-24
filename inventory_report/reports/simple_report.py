fc7793c51c4912816967d53c3962318a86f20119
danieleperse <danieleperse3@gmail.com>
2021-05-24 18:35:55 -0300

    Requisito 1 - primeiro commit - Daniele e Mariano

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,41 @@
+from datetime import date
+from collections import Counter
+
+
+class SimpleReport:
+    @staticmethod
+    def generate(lista_produtos):
+        fabricacao = SimpleReport.data_fabricacao_antiga(lista_produtos)
+        validade = SimpleReport.data_validade_proxima(lista_produtos)
+        empresa = SimpleReport.empresa_maior_estoque(lista_produtos)
+
+        return (
+            f"Data de fabricação mais antiga: {fabricacao}\n"
+            + f"Data de validade mais próxima: {validade}\n"
+            + "Empresa com maior quantidade de produtos estocados: "
+            f"{empresa}\n"
+        )
+
+    def data_validade_proxima(lista_produtos):
+        data_atual = date.today()
+        data_validade = []
+        for produto in lista_produtos:
+            if date.fromisoformat(produto["data_de_validade"]) > data_atual:
+                data_validade.append(produto["data_de_validade"])
+        data_validade.sort()
+        return data_validade[0]
+
+    def data_fabricacao_antiga(lista_produtos):
+        data_fabricacao = []
+        for produto in lista_produtos:
+            data_fabricacao.append(produto["data_de_fabricacao"])
+        data_fabricacao.sort()
+        return data_fabricacao[0]
+
+    def empresa_maior_estoque(lista_produtos):
+        empresas = []
+        for produto in lista_produtos:
+            empresas.append(produto["nome_da_empresa"])
+        ocorrencias = Counter(empresas)
+        mais_frequente = ocorrencias.most_common(1)[0][0]
+        return mais_frequente
