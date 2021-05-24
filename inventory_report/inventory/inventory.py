6807bbe1e09030f25f20d0a569d2a8694a6aa7a7
William Gomes <williamjog@hotmail.com>
2021-05-24 17:45:50 -0300

    fix: tentando corrigir 3º Requisito

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,18 +5,18 @@ from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
 
-def read_data_csv(filepath):
+def read_csv(filepath):
     with open(filepath) as file:
         csv_data = csv.DictReader(file)
         return list(csv_data)
 
 
-def read_data_json(filepath):
+def read_json(filepath):
     with open(filepath) as file:
         return json.load(file)
 
 
-def read_data_xml(file):
+def read_xml(file):
     root = element_tree.parse(file).getroot()
     data_tree = []
     for record in root:
@@ -31,13 +31,13 @@ class Inventory:
     @classmethod
     def import_data(self, test, type):
         if test.endswith(".csv"):
-            data = read_data_csv(test)
+            new_report = read_csv(test)
         if test.endswith(".json"):
-            data = read_data_json(test)
+            new_report = read_json(test)
         if test.endswith(".xml"):
-            data = read_data_xml(test)
+            new_report = read_xml(test)
         if type == "completo":
-            new_report = CompleteReport.generate(data)
+            new_report = CompleteReport.generate(new_report)
         else:
-            new_report = SimpleReport.generate(data)
+            new_report = SimpleReport.generate(new_report)
         return new_report
