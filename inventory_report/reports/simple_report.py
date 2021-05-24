dfc2772be2d266eba9d6031de83ce737f73afbb2
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2021-05-24 17:11:59 -0300

    req 1

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+
+
+class SimpleReport:
+    def generate(list):
+        today = datetime.today().strftime('%Y-%m-%d')
+        older = list[0]["data_de_fabricacao"]
+        validity_list = [today]
+        company_list = []
+        init = 0
+        for info in list:
+            factory = info["data_de_fabricacao"]
+            validity = info["data_de_validade"]
+            company = info["nome_da_empresa"]
+            company_list.append(company)
+            validity_list.append(validity)
+            if (factory < older):
+                older = factory
+
+        for names in company_list:
+            count = company_list.count(names)
+            if (count > init):
+                init = count
+                name = names
+
+        sort = sorted(validity_list)
+        index = sort.index(today)
+        closest = sort[index + 1]
+
+        return (
+            "Data de fabricação mais antiga: %s\n"
+            "Data de validade mais próxima: %s\n"
+            "Empresa com maior quantidade de produtos estocados: %s\n"
+            % (older, closest, name)
+        )
