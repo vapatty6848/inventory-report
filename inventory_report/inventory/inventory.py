02dd9a7430d7c87c71a7af26a20b43990746f142
thalesgr <thalesgr@gmail.com>
2021-05-24 18:31:33 -0300

    ate o req 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,14 +8,33 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @classmethod
     def import_data(cls, path, report_type):
+        if path.endswith(".csv"):
+            data = Inventory.csv_open(path)
+        if path.endswith(".json"):
+            data = Inventory.json_open(path)
+        if path.endswith('xml'):
+            data = Inventory.xml_open(path)
+       
+        if report_type == 'simples':
+            return SimpleReport.generate(data)
 
-        with open(path) as file:
-            csv_reader = csv.DictReader(file)
-            data = list(csv_reader)
+        if report_type == 'completo':
+            return CompleteReport.generate(data)
 
-            if report_type == 'simples':
-                return SimpleReport.generate(data)
+    @staticmethod
+    def csv_open(filepath):
+        with open(filepath) as file:
+            data = csv.DictReader(file)
+            return list(data)
 
-            if report_type == 'completo':
-                return CompleteReport.generate(data)
-
+    @staticmethod
+    def json_open(filepath):
+        with open(filepath) as file:
+            return json.load(file)
+    
+    @staticmethod
+    def xml_open(filepath):
+        with open(filepath) as file:
+            xml_dic = xmltodict.parse(file.read())
+            xml_json = json.dumps(xml_dic)
+            return json.loads(xml_json)['dataset']['record']
