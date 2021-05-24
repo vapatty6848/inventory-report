29a967b700ece1567f07b59317eaa1b591c7f1ec
thalesgr <thalesgr@gmail.com>
2021-05-24 15:09:00 -0300

    req 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,1 +1,24 @@
-# pra subir e abrir o pr
+from datetime import date
+
+
+class SimpleReport:
+    @classmethod
+    def generate(cls, inventory):
+        today = str(date.today())
+
+        oldest = min([item["data_de_fabricacao"] for item in inventory])
+        validade = min(
+            [
+                item["data_de_validade"]
+                for item in inventory
+                if item["data_de_validade"] >= today
+            ]
+        )
+        max_company = max(item["nome_da_empresa"] for item in inventory)
+
+        return (
+            f"Data de fabricação mais antiga: {oldest}\n"
+            f"Data de validade mais próxima: {validade}\n"
+            f"Empresa com maior quantidade de produtos estocados: "
+            f"{max_company}\n"
+        )
