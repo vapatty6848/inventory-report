04683bc4f24f227ab999d60477d8c76d3dfa0366
RenatoCostaDev <renatotrybe@gmail.com>
2021-05-25 17:48:54 -0300

    feat:Requisito-4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,15 +1,25 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
+
 # https://docs.python.org/3/library/csv.html
 
 
 class Inventory:
-    @classmethod
-    def import_data(cls, data_path, data_type):
-        with open(data_path) as data:
-            data_reader = csv.DictReader(data)
-            array_data = [data for data in data_reader]
-            if data_type == 'simples':
-                return SimpleReport.generate(array_data)
-            return CompleteReport.generate(array_data)
+    @staticmethod
+    def import_data(data_path, data_type):
+        data_path_end = data_path.split(".")[1]
+        if data_path_end == 'csv':
+            with open(data_path) as data:
+                data_reader = csv.DictReader(data)
+                array_data = [data for data in data_reader]
+                if data_type == "simples":
+                    return SimpleReport.generate(array_data)
+                return CompleteReport.generate(array_data)
+        elif data_path_end == 'json':
+            with open(data_path) as data:
+                array_data = json.load(data)
+                if data_type == "simples":
+                    return SimpleReport.generate(array_data)
+                return CompleteReport.generate(array_data)
