abd5a6d86aef8157d2d1854ad9c038dfc82071e4
ricknois <rickssti@outlook.com>
2021-05-25 14:11:24 -0300

    :sparkles: req 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -11,12 +12,20 @@ class Inventory:
             return list(csv_file)
 
     @classmethod
+    def read_json(cls, path):
+        with open(path) as file:
+            json_file = json.load(file)
+            return json_file
+
+    @classmethod
     def import_data(cls, path, type):
         file_content = ""
         if path.endswith(".csv"):
             file_content = cls.read_csv(path)
+        if path.endswith(".json"):
+            file_content = cls.read_json(path)
         if type == "simples":
             file_content = SimpleReport.generate(file_content)
-        else:
+        elif type == "completo":
             file_content = CompleteReport.generate(file_content)
         return file_content
