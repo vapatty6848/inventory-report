d79e74814b6b22ff1ce33e7af086a3eb57f8171d
isabellavjs <isabellavjs@gmail.com>
2021-05-25 19:20:12 -0300

    Requirement 7

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,6 +1,3 @@
-from inventory_report.importer.csv_importer import CsvImporter
-from inventory_report.importer.json_importer import JsonImporter
-from inventory_report.importer.xml_importer import XmlImporter
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.inventory.inventory_iterator import InventoryIterator
@@ -9,25 +6,19 @@ from collections.abc import Iterable
 
 class InventoryRefactor(Iterable):
     def __init__(self, strategy):
-        self._strategy = strategy
-        self._iterable_data = []
+        self.importer = strategy
+        self.data = []
 
     def import_data(self, path, version):
-        self._iterable_data = self._strategy.import_data(path)
-        # self._iterable_data.append(list_inventory)
+        list_inventory = self.importer.import_data(path)
+        self.data.extend(list_inventory)
 
         report = (
-            CompleteReport.generate(self._iterable_data)
+            CompleteReport.generate(self.data)
             if version == "completo"
-            else SimpleReport.generate(self._iterable_data)
+            else SimpleReport.generate(self.data)
         )
         return report
 
     def __iter__(self):
-        return InventoryIterator(self._iterable_data)
-
-
-csv_instance = InventoryRefactor(CsvImporter)
-print(csv_instance.import_data('inventory_report/data/inventory.csv', 'completo'))
-json_instance = InventoryRefactor(JsonImporter)
-xml_instance = InventoryRefactor(XmlImporter)
+        return InventoryIterator(self.data)
