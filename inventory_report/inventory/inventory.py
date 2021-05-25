b67e7a853665f02e81372e4e4c12559b2935452e
Tiago Berwanger <berwangertiago@gmail.com>
2021-05-25 13:41:54 -0300

    requirement 3 - refactored

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -4,33 +4,22 @@ import csv
 
 
 class Inventory:
+    def __init__(self, csv_file, type):
+        self.csv_file = csv_file
+        self.type = type
+
     @classmethod
-    def import_data(cls, csv_file, type):
-        simple_report = SimpleReport
-        complete_report = CompleteReport
+    def read_csv(cls, csv_file):
         with open(csv_file) as csvfile:
             reader = csv.DictReader(csvfile)
-            csv_content = list(reader)
+            return list(reader)
+
+    @classmethod
+    def import_data(cls, csv_file, type):
+        csv_content = cls.read_csv(csv_file)
+        simple_report = SimpleReport
+        complete_report = CompleteReport
         if type == "simples":
             return simple_report.generate(csv_content)
         if type == "completo":
             return complete_report.generate(csv_content)
-
-
-#     def __init__(self, csv_file, type):
-#         self.csv_file = csv_file
-#         self.type = type
-
-#     def read_csv(self, csv_file):
-#         with open(csv_file) as csvfile:
-#             reader = csv.DictReader(csvfile)
-#             return list(reader)
-
-#     def import_data(self, csv_file, type):
-#         csv_content = self.read_csv(Inventory, csv_file)
-#         simple_report = SimpleReport
-#         complete_report = CompleteReport
-#         if type == "simples":
-#             return simple_report.generate(csv_content)
-#         if type == "completo":
-#             return complete_report.generate(csv_content)
