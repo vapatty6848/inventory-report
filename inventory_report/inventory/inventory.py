1933b961632ed27ee57b875e15c6f9eb2188397d
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-05-25 19:12:22 -0300

    feat(inventory): add json to class inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,13 +7,27 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @classmethod
     def import_data(self, pathfile, type_report):
-        with open(pathfile) as file:
-            reporter_csv = csv.DictReader(file)
-            reporter = []
-            for row in reporter_csv:
-                reporter.append(row)
+        if pathfile[len(pathfile) - 1] == 'v':
+            with open(pathfile) as file:
+                reporter_csv = csv.DictReader(file)
+                reporter = []
+                for row in reporter_csv:
+                    reporter.append(row)
 
-            if type_report == 'simples':
-                return SimpleReport.generate(reporter)
-            else:
-                return CompleteReport.generate(reporter)
+                if type_report == 'simples':
+                    return SimpleReport.generate(reporter)
+                else:
+                    return CompleteReport.generate(reporter)
+
+        else:
+            with open(pathfile) as file:
+                content = file.read()
+                reporter_json = json.loads(content)
+
+                if type_report == 'simples':
+                    return SimpleReport.generate(reporter_json)
+                else:
+                    return CompleteReport.generate(reporter_json)
+
+
+# print(Inventory.import_data('/home/cleyton/Documents/Projects/computer-science/sd-06-inventory-report/inventory_report/data/inventory.json', 'simples'))
