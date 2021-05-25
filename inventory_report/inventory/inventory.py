9a3158fa5e2ffd251d3379f18b122a38e79e5e97
Alvaro Neto <alvaroshn@gmail.com>
2021-05-25 11:21:34 -0300

    Montar relatorio de arquivo json req 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -10,15 +11,34 @@ def read_csv(caminho):
         return list(relatorio_csv)
 
 
+def read_json(caminho):
+    '''Lê um arquivo JSON o qual o caminho é passado como parâmetro'''
+    with open(caminho) as file:
+        return json.load(file)
+
+
+def choose_reader(caminho):
+    '''Verifica qual a extensão do arquivo antes de ler o relatório'''
+    if caminho.endswith(".csv"):
+        return read_csv(caminho)
+    elif caminho.endswith(".json"):
+        return read_json(caminho)
+    else:
+        return False
+
+
 class Inventory:
     @classmethod
     def import_data(self, caminho, tipo_de_relatorio):
         '''Gera um relatorio a partir de um arquivo'''
-        relatorio = read_csv(caminho)
-        if tipo_de_relatorio == "simples":
-            meu_relatorio = SimpleReport.generate(relatorio)
-        elif tipo_de_relatorio == "completo":
-            meu_relatorio = CompleteReport.generate(relatorio)
+        relatorio = choose_reader(caminho)
+        if relatorio is not False:
+            if tipo_de_relatorio == "simples":
+                meu_relatorio = SimpleReport.generate(relatorio)
+            elif tipo_de_relatorio == "completo":
+                meu_relatorio = CompleteReport.generate(relatorio)
+            else:
+                meu_relatorio = "Falha ao gerar"
+            return meu_relatorio
         else:
-            meu_relatorio = ""
-        return meu_relatorio
+            return "Algo deu errado"
