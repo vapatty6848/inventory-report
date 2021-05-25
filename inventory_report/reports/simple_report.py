898a33df9b59d0c1938fa40b4f6b86a87c995ba3
DehSartoreto <fixsartoreto@gmail.com>
2021-05-25 15:21:47 -0300

    Req1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,8 +2,23 @@ from datetime import date
 
 
 class SimpleReport:
-  @classmethod
-    def generate(class, inventory):
-      date_today = str(date.today())
+    @classmethod
+    def generate(cls, inventory):
+        date_today = str(date.today())
 
-      oldest_item = min([item["data_de_fabricacao"] for item in inventory])
+        oldest_item = min([item["data_de_fabricacao"] for item in inventory])
+        product_validate = min(
+          [
+            item["data_de_validade"]
+            for item in inventory
+            if item["data_de_validade"] >= date_today
+          ]
+        )
+        max_company = max(item["nome_da_empresa"] for item in inventory)
+
+        return (
+          f"Data de fabricação mais antiga: {oldest_item}\n"
+          f"Data de validade mais próxima: {product_validate}\n"
+          f"Empresa com maior quantidade de produtos estocados: "
+          f"{max_company}\n"
+        )
