897afc59540bd3b18c7c57aa1a70652e72f6ced6
danieleperse <danieleperse3@gmail.com>
2021-05-25 16:05:48 -0300

    req. 3 e req. 4

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,41 @@
+import csv
+import json
+from xml_to_dict import XMLtoDict
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    def import_data(path, tipoRelatorio):
+
+        if path.split(".", 1)[1] == "csv":
+            relatorio = Inventory.leitor_csv(path)
+        if path.split(".", 1)[1] == "json":
+            relatorio = Inventory.leitor_json(path)
+        if path.split(".", 1)[1] == "xml":
+            relatorio = Inventory.leitor_xml(path)
+
+        if tipoRelatorio == "simples":
+            return SimpleReport.generate(relatorio)
+        else:
+            return CompleteReport.generate(relatorio)
+
+    def leitor_csv(path):
+        lista_produtos = []
+        with open(path, newline="") as file:
+            csv_file = csv.DictReader(file)
+            for row in csv_file:
+                lista_produtos.append(row)
+        return lista_produtos
+
+    def leitor_json(path):
+        with open(path) as file:
+            content = json.load(file)
+            return content
+
+    def leitor_xml(path):
+        with open(path) as file:
+            parser = XMLtoDict()
+            content = parser.value_from_nest(".*record", file)
+            # content = parser.parse(file)
+            return content
