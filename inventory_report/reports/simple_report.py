cdeef70f8669c46633b9cf83de4309abb2997e0f
Gustavo Oliva <gustavociviloliva@gmail.com>
2021-05-24 23:41:28 -0300

    :rotating_light: changing variable names

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -5,11 +5,10 @@ from datetime import date
 class SimpleReport:
     @classmethod
     def products_per_company(cls, products_list):
-        nome_empresa = []
-        for nome in products_list:
-            nome_empresa.append(nome["nome_da_empresa"])
-        listagem = Counter(nome_empresa)
-        return listagem
+        company_and_quantity = []
+        for product in products_list:
+            company_and_quantity.append(product["nome_da_empresa"])
+        return Counter(company_and_quantity)
 
     @classmethod
     def generate(cls, products_list):
@@ -17,21 +16,25 @@ class SimpleReport:
         base_date = 10000
         data_fabricacao = "2021-05-20"
         for product in products_list:
-            if(product["data_de_fabricacao"] < data_fabricacao):
+            if product["data_de_fabricacao"] < data_fabricacao:
                 data_fabricacao = product["data_de_fabricacao"]
-            validate = product["data_de_validade"].replace('-', ',').split(',')
+            validate = product["data_de_validade"].replace("-", ",").split(",")
             year = int(validate[0])
             month = int(validate[1])
             day = int(validate[2])
-            difference = date(year, month, day) - date_today
-            if(difference.days < base_date) and (difference.days > 0):
+            date_difference = date(year, month, day) - date_today
+            if (date_difference.days < base_date) and (
+                date_difference.days > 0
+            ):
                 data_validade = product["data_de_validade"]
-                base_date = difference.days
+                base_date = date_difference.days
 
         list_of_companies = cls.products_per_company(products_list)
         most_common_company = list_of_companies.most_common(1)[0][0]
 
-        return (f"Data de fabricação mais antiga: {data_fabricacao}\n"
-                + f"Data de validade mais próxima: {data_validade}\n"
-                + "Empresa com maior quantidade de produtos estocados: "
-                + f"{most_common_company}\n")
+        return (
+            f"Data de fabricação mais antiga: {data_fabricacao}\n"
+            + f"Data de validade mais próxima: {data_validade}\n"
+            + "Empresa com maior quantidade de produtos estocados: "
+            + f"{most_common_company}\n"
+        )
