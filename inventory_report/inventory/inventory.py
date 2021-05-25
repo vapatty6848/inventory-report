72bcfd7fec7d7d2e9cbd72bdf07df8e996da42f8
DehSartoreto <fixsartoreto@gmail.com>
2021-05-25 16:41:37 -0300

    Req 4 JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -7,12 +8,24 @@ class Inventory:
     @classmethod
     def import_data(cls, path, report):
 
-        with open(path) as file:
-            csv_reader = csv.DictReader(file)
-            data = list(csv_reader)
+        if path.endswith(".csv"):
+            data = Inventory.csv_open(path)
+        if path.endswith(".json"):
+            data = Inventory.json_open(path)
 
-            if report == 'simples':
-                return SimpleReport.generate(data)
+        if report == 'simples':
+            return SimpleReport.generate(data)
 
-            if report == 'completo':
-                return CompleteReport.generate(data)
+        if report == 'completo':
+            return CompleteReport.generate(data)
+
+    @staticmethod
+    def csv_open(filepath):
+        with open(filepath) as file:
+            data = csv.DictReader(file)
+            return list(data)
+
+    @staticmethod
+    def json_open(filepath):
+        with open(filepath) as file:
+            return json.load(file)
