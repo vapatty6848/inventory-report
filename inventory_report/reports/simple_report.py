6f13d2054c9ad01e1b4f705a20b2b2533d9e4bfb
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-05-26 14:20:40 -0300

    Subindo requisito 1

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,3 +1,27 @@
+from datetime import datetime
+
+
 class SimpleReport:
-    def __init__(self):
-        print("Só para iniciar o projeto")
+    @classmethod
+    def generate(self, produtos):
+        fabricacao_mais_antiga = str(min(
+             datetime.strptime(produto["data_de_fabricacao", "%Y-%m-%d"])
+             for produto in produtos
+         ).date())
+
+        proxima_validade = str(min(
+            datetime.strptime(produto["data_de_validade", "%Y-%m-%d"])
+            for produto in produtos
+            if datetime.strptime(produto["data_de_validade"], "%Y-%m-%d")
+            > datetime.today()
+            ).date())
+
+        maior_estoque = max(produto["nome_da_empresa"] for produto in produtos)
+
+        relatorio = (
+            + f"Data de fabricação mais antiga: {fabricacao_mais_antiga}\n"
+            + f"Data de validade mais próxima: {proxima_validade}\n"
+            "Empresa com maior quantidade de produtos estocados: "
+            + f"{maior_estoque}\n"
+        )
+        return relatorio
