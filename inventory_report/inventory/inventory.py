662d52e21cc830dfeda6058d6d83e834199a8f84
RenatoCostaDev <renatotrybe@gmail.com>
2021-05-26 13:25:06 -0300

    fix:Requisito-5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -13,6 +13,44 @@ class Inventory:
     def import_data(data_path, data_type):
         data_path_end = data_path.split(".")[1]
         if data_path_end == "csv":
+            return Inventory._csv_Import_Data(data_path, data_type)
+        if data_path_end == "json":
+            return Inventory._json_Import_Data(data_path, data_type)
+        if data_path_end == "xml":
+            return Inventory._xml_Import_Data(data_path, data_type)
+
+    @staticmethod
+    def _csv_Import_Data(data_path, data_type):
+        with open(data_path) as data:
+            data_reader = csv.DictReader(data)
+            array_data = [data for data in data_reader]
+            if data_type == "simples":
+                return SimpleReport.generate(array_data)
+            return CompleteReport.generate(array_data)
+
+    @staticmethod
+    def _json_Import_Data(data_path, data_type):
+        with open(data_path) as data:
+            array_data = json.load(data)
+            if data_type == "simples":
+                return SimpleReport.generate(array_data)
+            return CompleteReport.generate(array_data)
+
+    @staticmethod
+    def _xml_Import_Data(data_path, data_type):
+        with open(data_path) as data:
+            array_data = xmltodict.parse(data.read())["dataset"]["record"]
+            if data_type == "simples":
+                return SimpleReport.generate(array_data)
+            return CompleteReport.generate(array_data)
+
+
+'''
+class Inventory:
+    @staticmethod
+    def import_data(data_path, data_type):
+        data_path_end = data_path.split(".")[1]
+        if data_path_end == "csv":
             with open(data_path) as data:
                 data_reader = csv.DictReader(data)
                 array_data = [data for data in data_reader]
@@ -30,3 +68,4 @@ class Inventory:
             if data_type == "simples":
                 return SimpleReport.generate(array_data)
             return CompleteReport.generate(array_data)
+'''
