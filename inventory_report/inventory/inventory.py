7d84eb6c1c350728b725ace09f47099551d3510b
bynho25 <pablo.trybe@gmail.com>
2021-05-26 00:03:34 -0300

    Fix req 3 and try 4 and 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,11 +1,40 @@
-from inventory_report.importer.importer import Importer
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 import json
+import csv
+import xmltodict
 
 
-class JsonImporter(Importer):
+class Inventory:
     @classmethod
-    def import_data(self, filepath):
-        if not filepath.endswith(".json"):
-            raise ValueError("Arquivo inválido")
-        with open(filepath) as archive:
-            return json.load(archive)
+    def read_csv(cls, csv_file):
+        with open(csv_file) as csvfile:
+            reader = csv.DictReader(csvfile)
+            return list(reader)
+    
+    @classmethod
+    def read_json(cls, json_file):
+        with open(json_file) as jsonfile:
+          reader = json.load(jsonfile)
+          return list(reader)
+
+    @classmethod
+    def read_xml(cls, xml_file):
+        with open(xml_file, 'r') as xml_file:
+            data = xmltodict.parse(xmlfile.read())
+            json = json.dumps(data)
+            return json.loads(json)['dataset']['record']
+
+    @classmethod
+    def import_data(cls, file, type):
+        reader = {
+          "csv": Inventory.read_csv,
+          "json": Inventory.read_json,
+          "xml": Inventory.read_xml
+        }
+        extension = file.split('.')[1]
+        res = reader[extension](file)
+        if type == "simples":
+            return SimpleReport.generate(res)
+        if type == "completo":
+            return CompleteReport.generate(res)
